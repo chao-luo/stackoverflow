837724e509377cda580eb86f20dca4edbeb74e7f
Chao Luo (chaoluo) <chaoluo@cisco.com>
2019-02-27 23:22:03 +0800

    feature: init

--- src/main/java/com/questions/stackoverflow/StackOverflowApplication.java
+++ src/main/java/com/questions/stackoverflow/StackOverflowApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StackoverflowApplication {
+public class StackOverflowApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StackoverflowApplication.class, args);
+		SpringApplication.run(StackOverflowApplication.class, args);
 	}
 
 }
